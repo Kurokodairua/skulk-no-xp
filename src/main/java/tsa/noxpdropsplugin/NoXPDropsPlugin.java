7db217183f9ff4ff5efd4e8519b89f2f44ea6fc2
Tessa <stteleps@stud.h-da.de>
2023-07-15 00:02:20 +0200

    mit permissions

--- src/main/java/tsa/noxpdropsplugin/NoXPDropsPlugin.java
+++ src/main/java/tsa/noxpdropsplugin/NoXPDropsPlugin.java
@@ -8,11 +8,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoXPDropsPlugin extends JavaPlugin implements Listener {
 
     private boolean disableXPDrops = false;
+    private Permission skulkPermission = new Permission("noxpdrops.use");
+
 
     @Override
     public void onEnable() {
@@ -38,6 +41,10 @@ public class NoXPDropsPlugin extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("skulk")) {
+            if (!sender.hasPermission(skulkPermission)) {
+                sender.sendMessage("Du hast keine Berechtigung, diesen Befehl zu verwenden.");
+                return true;
+            }
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("on")) {
                     disableXPDrops = false;
@@ -51,7 +58,7 @@ public class NoXPDropsPlugin extends JavaPlugin implements Listener {
             }
         }
         else if (command.getName().equalsIgnoreCase("skulkinfo")) {
-            sender.sendMessage("geht"); // :)
+            sender.sendMessage("Ich hab da so ein Plugin geschrieben, LG Kuro"); // :)
         }
         return false;
     }
