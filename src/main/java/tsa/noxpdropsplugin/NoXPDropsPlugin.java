966879e948dbe3e9c26fd0e886db015bb53c0c62
Tessa <stteleps@stud.h-da.de>
2023-07-15 00:20:12 +0200

    ka alles ist doof :(

--- src/main/java/tsa/noxpdropsplugin/NoXPDropsPlugin.java
+++ src/main/java/tsa/noxpdropsplugin/NoXPDropsPlugin.java
@@ -13,10 +13,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoXPDropsPlugin extends JavaPlugin implements Listener {
 
-    private boolean disableXPDrops = false;
-    private Permission skulkPermission = new Permission("noxpdrops.use");
-
-
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -33,32 +29,15 @@ public class NoXPDropsPlugin extends JavaPlugin implements Listener {
         Block block = event.getBlock();
 
         // Überprüfen, ob es sich um einen Skulkblock oder Skulkvein handelt
-        if ((block.getType() == Material.SCULK || block.getType() == Material.SCULK_VEIN) && disableXPDrops) {
+        if ((block.getType() == Material.SCULK || block.getType() == Material.SCULK_VEIN)) {
             event.setExpToDrop(0); // Setze die Anzahl der abzuwerfenden Erfahrungspunkte auf 0
-            block.getWorld().spawn(block.getLocation(), ExperienceOrb.class).setExperience(0); // Spawnen eines Erfahrungspunkte-Orbs mit 0 XP
+           // block.getWorld().spawn(block.getLocation(), ExperienceOrb.class).setExperience(0); // Spawnen eines Erfahrungspunkte-Orbs mit 0 XP
         }
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("skulk")) {
-            if (!sender.hasPermission(skulkPermission)) {
-                sender.sendMessage("Du hast keine Berechtigung, diesen Befehl zu verwenden.");
-                return true;
-            }
-            if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("on")) {
-                    disableXPDrops = false;
-                    sender.sendMessage("Erfahrungspunkt-Drops für Skulkblöcke und Skulkveins sind aktiviert.");
-                    return true;
-                } else if (args[0].equalsIgnoreCase("off")) {
-                    disableXPDrops = true;
-                    sender.sendMessage("Erfahrungspunkt-Drops für Skulkblöcke und Skulkveins sind deaktiviert.");
-                    return true;
-                }
-            }
-        }
-        else if (command.getName().equalsIgnoreCase("skulkinfo")) {
+        if (command.getName().equalsIgnoreCase("skulkinfo")) {
             sender.sendMessage("Ich hab da so ein Plugin geschrieben, LG Kuro"); // :)
         }
         return false;
